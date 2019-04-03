7c0314508d785d056bf6d009cf9d187c43248325
J. Michael Welsh <mike.welsh@arm.com>
2019-04-03 16:46:21 -0500

    Adding license headers to files.
    Signed-off-by: J. Michael Welsh <mike.welsh@arm.com>

--- exampleDevices/temperatureSensor.js
+++ exampleDevices/temperatureSensor.js
@@ -1,7 +1,5 @@
 /*
- * ----------------------------------------------------------------------------
- * Copyright 2018 ARM Ltd.
- *
+ * Copyright (c) 2018, Arm Limited and affiliates.
  * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,7 +13,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ----------------------------------------------------------------------------
  */
 
 const util = require('util')
